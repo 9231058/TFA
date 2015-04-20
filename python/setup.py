5707e0bed6b15c8008512bd5e34b1d8f5ae28a2c
Parham Alvani <parham.alvani@gmail.com>
2015-04-21 01:01:19 +0430

    setup script is added.

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='TFA',
+    version='0.9',
+    packages=['fa'],
+    url='www.github.com/1995parham/TFA',
+    license='GPLv2',
+    author='Parham Alvani',
+    author_email='parham.alvani@gmail.com',
+    description='Tiny Finite Automata, simple library for working with FAs. '
+)
