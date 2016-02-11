4bca584c5604546d2bc44c7e5f438e4131d826c1
Parham Alvani <parham.alvani@gmail.com>
2016-02-11 09:06:58 +0330

    State structure was implemented.

---
+++ c/src/state.c
@@ -0,0 +1,30 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : state.c
+ *
+ * [] Creation Date : 11-02-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+#include "state.h"
+
+#include <stdlib.h>
+
+struct state *state_new(const void *data)
+{
+	struct state *new = malloc(sizeof(struct state));
+
+	new->data = data;
+
+	return new;
+}
+
+void state_delete(struct state *s)
+{
+	free(s);
+}
