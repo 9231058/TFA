8a6eea01ff519f4b4297405db1561e728ce1b1a1
Parham Alvani <parham.alvani@gmail.com>
2016-02-11 08:57:12 +0330

    Abstract class of DFAs was added.

---
+++ c/src/dfa.c
@@ -0,0 +1,31 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : dfa.c
+ *
+ * [] Creation Date : 11-02-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+#include <stdlib.h>
+
+#include "dfa.h"
+
+struct dfa *dfa_new(void *init_data)
+{
+	struct dfa *new = malloc(sizeof(struct dfa));
+	
+	new->c = dfa_init(new, init_data);
+
+	return new;
+}
+
+void dfa_process(struct dfa *d)
+{
+	while (!d->c->is_final)
+		d->c = dfa_transition(d);
+}
---
+++ c/src/dfa.h
@@ -0,0 +1,48 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : dfa.h
+ *
+ * [] Creation Date : 11-02-2016
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+/*
+ * Copyright (c) 2016 Parham Alvani.
+*/
+
+struct state {
+	const void *data;
+	int is_final;
+};
+
+struct dfa {
+	const struct state *c;
+};
+
+/*
+ * DFA implemented function
+ * create DFA just for YOU :)
+*/
+struct dfa *dfa_new(void *init_data);
+
+/*
+ * DFA to be implemented function
+ * call in dfa_new for get initial state
+ * you can use init_data in order to give it
+ * needed information
+*/
+const struct state *dfa_init(const struct dfa *d, void *init_data);
+
+/*
+ * DFA implemented funtion
+ * block until DFA process completely to final state
+*/
+void dfa_process(struct dfa *d);
+
+/*
+ * DFA to be implemented function
+ * call in dfa_process for state transition
+*/
+const struct state *dfa_transition(const struct dfa *d);
